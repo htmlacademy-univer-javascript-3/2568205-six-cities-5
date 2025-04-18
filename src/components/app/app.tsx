f7819c48865743c6ce03396661d1bdc5d458d75c
Artyom <artp1905@ya.ru>
2025-04-18 17:22:21 +0500

    Add routes

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,11 +1,27 @@
+import Login from "../../pages/login/login";
 import Main from "../../pages/main/main"
+import {Route, BrowserRouter, Routes} from 'react-router-dom';
+import Offer from "../../pages/offer/offer";
+import Favorites from "../../pages/favorites/favorites";
+import NotFound from "../../pages/not-found/not-found";
+import { AppRoute, AuthorizationStatus } from "../../const";
+import PrivateRoute from "../private-route/private-route";
 
 type AppProps = {
     placesCount: number
 }
 function App({placesCount}: AppProps) : JSX.Element {
 return (
-    <Main placesCount={placesCount}/>
+   <BrowserRouter>
+   <Routes>
+   <Route path={AppRoute.Root} element={<Main placesCount={placesCount} />}/>
+   <Route path={AppRoute.Login} element={<Login />}/>
+   <Route path={AppRoute.Offer} element={<Offer/>}/>
+   <Route path={AppRoute.Favorites} element={
+    <PrivateRoute children={<Favorites />} authorizationStatus={AuthorizationStatus.NoAuth}/>}/>
+   <Route path="*" element={<NotFound />}/>
+   </Routes>
+   </BrowserRouter>
 )
 }
 export default App
---
+++ src/const.ts
@@ -0,0 +1,12 @@
+export enum AppRoute {
+    Login = '/login',
+    Offer = '/offer/:id',
+    Favorites = '/favorites',
+    Root = '/',
+  }
+  
+  export enum AuthorizationStatus {
+    Auth = 'AUTH',
+    NoAuth = 'NO_AUTH',
+    Unknown = 'UNKNOWN',
+  }
